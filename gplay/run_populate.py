9a8341f3e472ff50654b8b2df82f2191da96760c
enrevol <enrevol@gmail.com>
2016-08-15 17:08:26 +0700

    - add scripts.

---
+++ gplay/run_populate.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+import os, os.path
+
+cmd = "python ../../../store-listing-toolkit/populate-v2.py metadata -platform android -prj-path . -sheet 15WJ6dtUsROCkjmMaNj-BMp7CjOiFjB4B7MPD5hoHzq8 -customized-metadata-path ../src/gplay/metadata"
+print cmd
+os.system(cmd)
