1f237fc86714ad3b551c7ccf733f1410058a21f0
Duc Nguyen <ndpduc@gmail.com>
2018-09-13 13:55:07 +0700

    - Update file json name.

--- gplay/run_supply.py
+++ gplay/run_supply.py
@@ -4,6 +4,6 @@
 import sys
 import os, os.path
 
-cmd = 'supply -j ../../Google\ Play\ Android\ Developer-394f15d83386.json -p com.senspark.android.southernthirteen --skip_upload_apk true --skip_upload_images true --skip_upload_screenshots true'
+cmd = 'supply -j ../../Google\ Play\ Android\ Developer.json -p com.senspark.android.southernthirteen --skip_upload_apk true --skip_upload_images true --skip_upload_screenshots true'
 print cmd
 os.system(cmd)
