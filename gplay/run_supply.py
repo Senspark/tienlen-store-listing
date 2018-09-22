0abd7d75a87c741a9dc440f1209cd9ead0ecabb8
Duc Nguyen <ndpduc@gmail.com>
2018-09-22 15:38:12 +0700

    - update sheet.

--- gplay/run_supply.py
+++ gplay/run_supply.py
@@ -4,6 +4,6 @@
 import sys
 import os, os.path
 
-cmd = 'supply -j ../../Google\ Play\ Android\ Developer.json -p com.senspark.android.southernthirteen --skip_upload_apk true --skip_upload_images true --skip_upload_screenshots true'
+cmd = 'fastlane supply -j ../../Google\ Play\ Android\ Developer.json -p com.senspark.android.southernthirteen --skip_upload_apk true --skip_upload_images true --skip_upload_screenshots true'
 print cmd
 os.system(cmd)
--- itunes/populate_metadata.py
+++ itunes/populate_metadata.py
@@ -4,6 +4,6 @@
 import sys
 import os, os.path
 
-cmd = "python ../../../store-listing-toolkit/populate-v3.py metadata -platform iOS -prj-path . -sheet 15WJ6dtUsROCkjmMaNj-BMp7CjOiFjB4B7MPD5hoHzq8 -customized-metadata-path ../src/itunes/metadata"
+cmd = "python ../../../store-listing-toolkit/populate-v3.py metadata -platform iOS -prj-path . -sheet 1Dee0sr3A3P_ipYWOUCtrWaX-Onrx4-8DxOUOnbCs_5M -customized-metadata-path ../src/itunes/metadata"
 print cmd
 os.system(cmd)
